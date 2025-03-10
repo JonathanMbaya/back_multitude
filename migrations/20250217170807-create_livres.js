040969fde9e57f047636363b5a2970ad2d54375b
unknown <jonathanmbaya13@gmail.com>
2025-03-10 03:37:19 +0100

    Mise à jour Livre migrate

--- migrations/20250217170807-create_livres.js
+++ migrations/20250217170807-create_livres.js
@@ -29,7 +29,7 @@ module.exports = {
         type: Sequelize.DATE,
         allowNull: false,
       },
-      bandeAnnonce: {
+      cover: {
         type: Sequelize.STRING,
       },
       createdAt: {
